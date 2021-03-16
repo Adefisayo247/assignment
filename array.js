ddee13ff88a30455b5898345acbbd80a4b4a0b68
Adefisayo247 <76254168+Adefisayo247@users.noreply.github.com>
2021-03-16 09:53:13 +0000

    Add files via upload

---
+++ array.js
@@ -0,0 +1,23 @@
+//designer arrays
+const designer = [
+    ['fendi', 'gucci', 'louisVuitton'],
+    ['victoriaSecret', 'fenty', 'puma'],
+    ['addidas', 'skechers', 'nike']
+]
+console.log(designer[2][1])
+
+//countries arrays
+const countries = [
+    ['france', 'germany', 'russia', 'england'],
+    ['nigeria', 'uganda', 'southAfrica', 'namibia'],
+    ['argentina', 'brazil', 'chile', 'peru']
+]
+console.log(countries[3][0])
+
+//languages arrays
+const languages = [
+    ['english', 'portuguese', 'finnish', 'swedish'],
+    ['yoruba', 'hausa', 'igbo', 'igala'],
+    ['spanish', 'italian', 'russian', 'chinese']
+]
+console.log(languages[0][2])
---
+++ keyValue.js
@@ -0,0 +1,57 @@
+//basketballplayer profile
+const basketballPlayer = {
+    name: 'lebron James',
+    age: '36',
+    height: '6feet, 6inches',
+    weight: '85kg',
+    position: 'shooting guard',
+    averagePointPerGame: '34',
+    stealsPerGame: '8.4',
+    blocksPerGame: '4.2',
+    numberOfThreePoint: '102',
+    mvpCallUp: '15',
+    freeThrowPoint: '20',
+    sponsor: 'nike',
+        name: 'airNitrogen7',
+        shoeSize: 45,
+        color:['purple', 'orange', 'black'],
+        yearOfProduction: '2018',
+        warranty: '2years'
+}
+console.log(basketballPlayer.color[2])
+
+//designer Profile
+const designer = {
+    identity: "fendi",
+    yearEstablished: 1920,
+    networth: "two Billion Dollars",
+    countriesReached: 147,
+    numberOfEmployees: 20000,
+    product: ["bags", "clothes", "shoes"],
+    approachToMarket: "B2C",
+    nameOfCEO: "nobel Kretine",
+    headquater: "rome",
+    biggestMarket: ["lagos", "munich", "madrid", "rome"],
+}
+
+console.log(designer.biggestMarket[3])
+
+//planet earth profile
+const earth = {
+    distanceFromSun: '157000000km',
+    continents: ["europe", "africa", "australia"],
+    numberOfCountries: 207,
+    language: ["english", "german", "spanish", "yoruba"],
+            englishSpeakingCountries: 102,
+            portugueseSpeakingCountries: 15,
+            frenchSpeakingCountries: 50,
+            arabicSpeakingCountries: 30,
+            nativeSpeakingCountires: 10,
+    governmentPractice: ["military", "fascism", "communism", "nazism"],
+    population: "20million",
+    richestCountries: ["luxembourg", "switzerland", "macau", "norway"],
+    poorestCountries: ["rwanda", "cambodia", "mexico", "nigeria"],
+    landLockedArea: 103,
+    waterLoggedArea: 104,
+}
+console.log(earth.continents[1])
